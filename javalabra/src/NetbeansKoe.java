daae5efe4dad93e6e7c1439e0cc7fd4fabb2b9c2
lkalli <laura.kalli@helsinki.fi>
2012-11-07 02:10:57 +0200

    vaihdettu netbensissa kansiosta toiseen, tsekataan

---
+++ javalabra/src/NetbeansKoe.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author laura
+ */
+public class NetbeansKoe {
+    //public static void main(String[] args) {
+       // System.out.println("asdf");
+        
+        //toinen pushaustesti, nyt suoraan netbeansista
+        //vielä vika pushaustesti tästä kansiosta
+    
+    
+    }
+
